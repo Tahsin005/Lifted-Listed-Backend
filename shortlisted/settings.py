21bccfb4c6971e7fd7c00a5ca99794db34f30672
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-20 13:41:58 +0600

    csrf verfication

--- shortlisted/settings.py
+++ shortlisted/settings.py
@@ -18,7 +18,7 @@ DEBUG = True
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    # 'https://shortlisted.onrender.com',
+    'https://shortlisted-iol7.onrender.com',
     'https://*.127.0.0.1',
     'http://127.0.0.1:5500',
 ]
